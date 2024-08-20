6c135aed5ec5afdf9d22c2591f3a81b09f8c62cf
BuiThien <thienbuiduy.work@gmail.com>
2024-08-20 18:34:44 +0700

    [add] str str, longest common

---
+++ LeetCode_HackerRank/LongestCommonPrefix/longest-common-prefix.ts
@@ -0,0 +1,11 @@
+function longestCommonPrefix(strs: string[]): string {
+  const result = "";
+  const length = strs.length;
+  for (let i = 1; i < length; i++) {
+    if(strs[i][0] == strs[i-1][0]) {
+        
+    }
+  }
+
+  return result;
+}
